6b3b53ac65fa71b93f4ed7feeb8432ee9c6c1755
Damion Hardy <damionhardy@Damos-MacBook-Pro-8.local>
2018-10-31 12:27:06 +0000

    new db table

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,14 @@
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE posts (
   id bigserial PRIMARY KEY,
   content varchar(250) NOT NULL
+);
+
+CREATE TABLE users (
+  id bigserial PRIMARY KEY,
+  email varchar(64) UNIQUE,
+  username varchar(64) UNIQUE,
+  password varchar(64)
 );
