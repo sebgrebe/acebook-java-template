aa6afc633227c86d2a63a66369e66ddf6b57042f
Sebastian <sebbecker@gmx.net>
2018-10-31 11:11:38 +0000

    add user model

---
+++ src/main/java/com/makersacademy/acebook/model/user.java
@@ -0,0 +1,27 @@
+package com.makersacademy.acebook.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "USERS")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    private User() {}
+
+    public User(String username) {
+        this.username = username;
+    }
+
+}
