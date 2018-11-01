fea8afd84e56fa6c76711da26d1411befbbeac69
Victor Jefferies <victorjefferies@MacBook-Pro-9.local>
2018-11-01 15:35:08 +0000

    added state for signin

--- src/main/js/posts/signup.js
+++ src/main/js/posts/signup.js
@@ -4,16 +4,31 @@ import axios from 'axios';
 class Signup extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { showSuccess: false };
+    
+  }
+
+  toggleSignUp() {
+    var showSuccess = this.state.showSuccess;
+    this.setState({      
+      showSuccess: showSuccess == false ? true : false      
+    })
+   
   }
 
   logger(axiosLib = axios){
+    var that = this;
     let newUserInfo = {"email": document.getElementById("email").value, "username": document.getElementById("username").value, "password": document.getElementById("psw").value}
     // console.log(newUserInfo);
     axiosLib.post('api/signup', newUserInfo)
     .then(function(response) {
-      console.log(response.data)
+      console.log(`here is SS: ${that.state.showSuccess}`);
+      that.toggleSignUp();
+      console.log(`here is SS: ${that.state.showSuccess}`)
     })
-
+      .catch(function (error) {
+          console.log(error);
+      });
   }
 
 
