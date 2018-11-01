22211bb01287ba7a3852496221d0846516a69eb9
Sebastian <sebbecker@gmx.net>
2018-11-01 12:06:04 +0000

    encrypt pw

--- src/main/java/com/makersacademy/acebook/service/UserServiceImplementation.java
+++ src/main/java/com/makersacademy/acebook/service/UserServiceImplementation.java
@@ -3,15 +3,28 @@ package com.makersacademy.acebook.service;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.service.UserService;
 import com.makersacademy.acebook.repository.UserRepository;
+
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserServiceImplementation implements UserService {
+
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Autowired
     private UserRepository userRepository;
 
     public void addUser(User user) {
+      String encryptedPW = this.encryptPassword(user.getPassword());
+      user.setPassword(encryptedPW);
       userRepository.save(user);
     }
+
+    public String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
 }
